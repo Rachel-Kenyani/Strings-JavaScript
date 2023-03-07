269b94ec99cfb0a1ccf61255931b96af0b792e64
Rachel Kenyani <rachelkenyani@gmail.com>
2023-03-07 06:20:41 +0300

    first commit

---
+++ quiz.js
@@ -0,0 +1,43 @@
+let girl="beautiful";
+
+console.log("girl",girl[5]);
+console.log("girl",girl[6]);
+console.log("girl",girl[7]);
+console.log("girl",girl[8]);
+
+const food="I was feeling hungry today";
+let food2 = food.replace("today", "eat");
+
+console.log("food",food2);
+
+const story="The quick brown fox jumps over the lazy dog";
+let story2=story.count("the");
+let story3=story.count("brown");
+
+console.log("story",story2);
+console.log("story",story3);
+
+const string1 = "We are now going to school";
+ let a = string1.substring(7,10);
+console.log("string1",a);
+
+const string2 = "The child was sitting on the table before it fell";
+let b=string2.substring(14,21);
+console.log("string2",b);
+
+
+let c="wonderful";
+let result = c.toUpperCase();
+
+console.log("c",result);
+
+let d="amazing,BEneath";
+let result2 = d.toLowerCase();
+
+console.log("d",result2);
+
+let e="A beautiful wedding";
+let result3 = e.toUpperCase();
+
+console.log("e",result3);
+
